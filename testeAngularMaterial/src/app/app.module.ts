af6b9b1f90267547bab684416e65f651e99c634b
BlackCode7 <seven.metais@outlook.com>
2022-10-18 22:32:16 -0300

    Testes de angular com material

---
+++ testeAngularMaterial/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatSortModule} from '@angular/material/sort';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    MatSortModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
